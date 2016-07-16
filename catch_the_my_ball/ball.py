6369819b4bce8893fb75c70bb40183b87ff0d662
korobitzyna <korobitzyna.elya@yandex.ru>
2016-07-16 10:37:10 +0500

    шарик с двумя окнами

--- catch_the_my_ball/ball.py
+++ catch_the_my_ball/ball.py
@@ -5,6 +5,7 @@ ball_initial_number = 20
 ball_minimal_radius = 15
 ball_maximal_radius = 40
 ball_available_colors = ['green', 'blue', 'red', 'lightgray', '#FF00FF', '#FFFF00']
+ball_recruited_sum = 0
 
 def click_ball(event):
     """ Обработчик событий мышки для игрового холста canvas
@@ -17,6 +18,7 @@ def click_ball(event):
 
     if x1 <= event.x <= x2 and y1 <= event.y <= y2:
         canvas.delete(obj)
+        ball_recruited_sum += 1
         # FIXME: нужно учесть объект в очках
         create_random_ball()
 
@@ -63,9 +65,52 @@ def init_main_window():
     canvas.bind("<Motion>", move_all_balls)
     canvas.pack()
 
+def button1_command():
+    #обработка событий кнопки 1
+    print('Button 1 default command.')
+
+
+def print_hello(event):
+    #обработка событий кнопки м1 и кнопки 2
+    print(event.num)
+    print(event.x, event.y)
+    #print(event.x_root, event.y_root)
+    me = event.widget
+    # что можно сделать с me?
+    if me == button1:
+        print('Hello!')
+    elif me == button2:
+        print('You pressed button 2!')
+    else:
+        raise ValueError()
+def init_main_window2():
+    """
+ Инициализация второго окна: создание всех необходимых виджетов и их упаковка.
+ :return:
+    """
+    global root2, button1, button2, label, text, scale
+
+    root2 = tkinter.Tk()
+
+    button1 = tkinter.Button(root2, text="Button 1", command=button1_command)
+    button1.bind("<Button>", print_hello)
+
+    button2 = tkinter.Button(root2, text="Button 2")
+    button2.bind("<Button>", print_hello)
+
+    variable = tkinter.IntVar(0)
+    label = tkinter.Label(root2, textvariable=variable)
+    scale = tkinter.Scale(root2, orient=tkinter.HORIZONTAL, length=300,
+                          from_=0, to=100, tickinterval=10, resolution=5, variable=variable)
+    text = tkinter.Entry(root2, textvariable=variable)
+
+    for obj in button1, button2, label, scale, text:
+        obj.pack()
+
 
 if __name__ == "__main__":
     init_main_window()
+    init_main_window2()
     init_ball_catch_game()
     root.mainloop()
     print("Приходите поиграть ещё!")
