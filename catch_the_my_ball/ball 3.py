9ba4a2d975f6da8243b01f532bccf01bb0dfd669
korobitzyna <korobitzyna.elya@yandex.ru>
2016-07-17 09:30:01 +0500

    попытка вывести на дополнительный экран сумму и скорость появления шариков

--- catch_the_my_ball/ball 3.py
+++ catch_the_my_ball/ball 3.py
@@ -1,6 +1,7 @@
 import tkinter
 from random import choice, randint
 
+
 ball_initial_number = 20
 ball_minimal_radius = 15
 ball_maximal_radius = 40
@@ -17,7 +18,6 @@ def click_ball(event):
     """
     obj = canvas.find_closest(event.x, event.y)
     x1, y1, x2, y2 = canvas.coords(obj)
-    create_random_ball()
     if x1 <= event.x <= x2 and y1 <= event.y <= y2:
         canvas.delete(obj)
         create_random_ball()
@@ -44,12 +44,13 @@ def create_random_ball():
     создаёт шарик в случайном месте игрового холста canvas,
      при этом шарик не выходит за границы холста!
     """
-    global ball_recruited_sum
-    for i in range(ball_recruited_sum):
+    global ball_speed_decrease
+    for i in range(ball_speed_decrease):
         R = randint(ball_minimal_radius, ball_maximal_radius)
         x = randint(0, int(canvas['width'])-1-2*R)
         y = randint(0, int(canvas['height'])-1-2*R)
         canvas.create_oval(x, y, x+2*R, y+2*R, width=1, fill=random_color())
+
 
 
 def random_color():
@@ -78,8 +79,25 @@ def init_main_window():
     label.pack()
     canvas.pack()
 
+def init_window_2():
+    global root2, canvas2, label2, ball_recruited_sum, scala
+
+    root2 = tkinter.Toplevel()
+    canvas2 = tkinter.Canvas(root2, background='white', width=400, height=400)
+    variable = tkinter.IntVar(int(ball_recruited_sum/10))
+    label2 = tkinter.Label(root2, textvariable=variable)
+    scale = tkinter.Scale(root2, orient=tkinter.HORIZONTAL, length=300,
+                          from_=0, to=10, tickinterval=1, resolution=0.5, variable=variable)
+    text = tkinter.Entry(root2, textvariable=variable)
+
+    for obj in  label2, scale, text:
+        obj.pack()
+
+
 if __name__ == "__main__":
     init_main_window()
+    init_window_2()
     init_ball_catch_game()
+    root2.mainloop()
     root.mainloop()
     print("Приходите поиграть ещё! Вы набрали", ball_recruited_sum, "баллов")
