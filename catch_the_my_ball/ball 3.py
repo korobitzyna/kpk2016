03f2e0a883077137f7d56fbc5bddf0dac7c2ed16
korobitzyna <korobitzyna.elya@yandex.ru>
2016-07-17 11:50:24 +0500

    выводит на дополнительный экран сумму и скорость максимальный радиус появляется лишнее окно, радиус уходим в минус

--- catch_the_my_ball/ball 3.py
+++ catch_the_my_ball/ball 3.py
@@ -6,33 +6,35 @@ ball_initial_number = 20
 ball_minimal_radius = 15
 ball_maximal_radius = 40
 ball_available_colors = ['green', 'blue', 'red', 'lightgray', '#FF00FF', '#FFFF00']
-ball_recruited_sum = 1
-ball_speed_decrease = 1
-ball_speed_increases = 1
-
+ball_recruited_sum = 0
+ball_speed_decrease = 0
 def click_ball(event):
     """ Обработчик событий мышки для игрового холста canvas
     :param event: событие с координатами клика
     По клику мышкой нужно удалять тот объект, на который мышка указывает.
     А также засчитываеть его в очки пользователя.
     """
+    global ball_maximal_radius, ball_speed_decrease
     obj = canvas.find_closest(event.x, event.y)
     x1, y1, x2, y2 = canvas.coords(obj)
     if x1 <= event.x <= x2 and y1 <= event.y <= y2:
         canvas.delete(obj)
         create_random_ball()
-        # FIXME: генератор шариков сделать зависимым от набранной суммы
+        ball_maximal_radius -= 1 #максимальный радиус шарика уменьшается
+        if ball_minimal_radius >= ball_maximal_radius:
+            ball_speed_decrease -= 1 #скорость появления шариков уменьшается
+        else:
+            ball_speed_decrease += 1 #скорость появления шариков увеличивается
         ball_sum() # счетчик собранных шариков
-        # FIXME: нужно учесть объект в очках
 
 def ball_sum():
-    # подсчет баллов
+    # подсчет суммы баллов
     global ball_recruited_sum
     ball_recruited_sum += 1
 
 
 def move_all_balls(event):
-    """ передвигает все шарики на чуть-чуть
+    """ передвигает все шарики на чуть-чуть НО выходят за границы холста
     """
     for obj in canvas.find_all():
         dx = randint(-1, 1)
@@ -42,14 +44,17 @@ def move_all_balls(event):
 def create_random_ball():
     """
     создаёт шарик в случайном месте игрового холста canvas,
-     при этом шарик не выходит за границы холста!
+     при этом шарик не выходит за границы холста
     """
-    global ball_speed_decrease
-    for i in range(ball_speed_decrease):
-        R = randint(ball_minimal_radius, ball_maximal_radius)
-        x = randint(0, int(canvas['width'])-1-2*R)
-        y = randint(0, int(canvas['height'])-1-2*R)
-        canvas.create_oval(x, y, x+2*R, y+2*R, width=1, fill=random_color())
+    global ball_speed_decrease,  ball_minimal_radius, ball_maximal_radius
+    for i in range(ball_speed_decrease+1):
+        if ball_minimal_radius >= ball_maximal_radius:
+            ball_minimal_radius, ball_maximal_radius = ball_maximal_radius, ball_minimal_radius
+        else:
+            R = randint(ball_minimal_radius, ball_maximal_radius)
+            x = randint(0, int(canvas['width'])-1-2*R)
+            y = randint(0, int(canvas['height'])-1-2*R)
+            canvas.create_oval(x, y, x+2*R, y+2*R, width=1, fill=random_color())
 
 
 
@@ -68,36 +73,40 @@ def init_ball_catch_game():
         create_random_ball()
 
 def init_main_window():
-    global root, canvas, label
+    global root, canvas
 
     root = tkinter.Tk()
     canvas = tkinter.Canvas(root, background='white', width=400, height=400)
     canvas.bind("<Button>", click_ball)
     canvas.bind("<Motion>", move_all_balls)
     canvas.pack()
-    label = tkinter.Label(root, text=ball_recruited_sum, font="Arial 12")
-    label.pack()
     canvas.pack()
 
 def init_window_2():
-    global root2, canvas2, label2, ball_recruited_sum, scala
+    global root2, canvas2, label2, label3,ball_recruited_sum, scala
 
     root2 = tkinter.Toplevel()
-    canvas2 = tkinter.Canvas(root2, background='white', width=400, height=400)
-    variable = tkinter.IntVar(int(ball_recruited_sum/10))
-    label2 = tkinter.Label(root2, textvariable=variable)
-    scale = tkinter.Scale(root2, orient=tkinter.HORIZONTAL, length=300,
-                          from_=0, to=10, tickinterval=1, resolution=0.5, variable=variable)
-    text = tkinter.Entry(root2, textvariable=variable)
-
-    for obj in  label2, scale, text:
+    label2 = tkinter.Label(root2, text="Приходите поиграть ещё!")
+    label3_text = "Вы набрали " + str(ball_recruited_sum) +" баллов"
+    label3 = tkinter.Label(root2, text=label3_text)
+    label4_text = "Максимальный радиус " + str(ball_maximal_radius)
+    label4 = tkinter.Label(root2, text=label4_text)
+    for obj in label2, label3, label4:
         obj.pack()
+    root2.mainloop()
+
+def game_over():
+    # выводит итог игры
+    global ball_recruited_sum, ball_maximal_radius
+    init_window_2()
+    print("Приходите поиграть ещё!")
+    print("Вы набрали", ball_recruited_sum, "баллов")
+    print("максимальный радиус", ball_maximal_radius)
+    exit()
 
 
 if __name__ == "__main__":
     init_main_window()
-    init_window_2()
     init_ball_catch_game()
-    root2.mainloop()
     root.mainloop()
-    print("Приходите поиграть ещё! Вы набрали", ball_recruited_sum, "баллов")
+    game_over()
